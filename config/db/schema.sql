63e12fd1342e387a777bb6fab30b7991b4c5e3ec
Herney Cardenas <i.herneyc@gmail.com>
2020-07-06 21:25:00 -0700

    revised MySQL and some css

--- config/db/schema.sql
+++ config/db/schema.sql
@@ -4,20 +4,28 @@ CREATE DATABASE camp_app_db;
 USE camp_app_db;
 
 CREATE TABLE camperinfo (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  email varchar(50) NOT NULL,
-  userPassword varchar(20) NOT NULL,
-  state varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+     id int NOT NULL AUTO_INCREMENT,
+     name varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
+    userPassword varchar(20) NOT NULL,
+     state varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE campinglist
-(
+CREATE TABLE campinglist(
 	id int NOT NULL AUTO_INCREMENT,
 	items VARCHAR(45) NOT NULL,
 	got_it BOOLEAN DEFAULT false,
     need_it BOOLEAN DEFAULT false,
     add_shopping_cart BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
+);
+
+CREATE TABLE productlist (
+    id int NOT NULL AUTO_INCREMENT,
+    product VARCHAR(45) NULL, 
+    asin INTEGER NULL,
+    price DECIMAL(10,2) NULL,
+    quantity INTEGER NULL,
+    PRIMARY KEY (id)
 );
--- config/db/seeds.sql
+++ config/db/seeds.sql
@@ -3,3 +3,32 @@ INSERT INTO campinglist (items) VALUES ('Sleeping Bag');
 INSERT INTO campinglist (items) VALUES ('Stove');
 INSERT INTO campinglist (items) VALUES ('Water Filter');
 INSERT INTO campinglist (items) VALUES ('Dehydrated Food');
+
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Coleman Sundome Tent', 'B019N9W6EQ', 69.99);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Bessport 3 Person Tent', 'B07YKTCJL9', 99.99);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('BFULL Camping Tent', 'B082MJBNW1', 79.99);
+
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Oaskys Camping Sleeping Bag', 'B07MFTD2YS', 25.99);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Coleman Mummy Sleeping Bag', 'B0009PUSO4', 49.99);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('TETON Sports Celsius Sleeping Bag', 'B001D6MF26', 72.15);
+
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Coleman & Triton Camping Stove', 'B00QMVEG8S', 74.89);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Coleman 2 Burner Grill Stove Combo', 'B000W4VD8C', 95.79);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Coleman Protable Butane Stove', 'B00FGPXVSM', 26.19);
+
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Sawyer Products Mini Water Filter', 'B00FA2RLX2', 19.97);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('Survivor Filter PRO Water Filter', 'B00QFXGSIY', 64.95);
+INSERT INTO productlist (product, asin, price) 
+VALUES ('LifeStraw Flex Advanced Water Filter', 'B07J3NMN9L', 41.46);
+
